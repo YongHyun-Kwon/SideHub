94b468cbe5200b37fad9b8ee06a36786faed74eb
jeremiah <yhkwon0820@gmail.com>
2023-06-05 13:50:47 +0900

    Initial commit

---
+++ src/main/java/com/bj/sidehub/SidehubApplication.java
@@ -0,0 +1,13 @@
+package com.bj.sidehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bj/sidehub/SidehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bj.sidehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
